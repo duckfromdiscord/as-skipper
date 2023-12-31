c6f88c8e351685963c8b61094df8226c7b66b37b
duck <113956421+duckfromdiscord@users.noreply.github.com>
2023-12-31 12:39:16 -0500

    split skip into a separate function

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,16 +8,14 @@
 #include <string>
 #include <sstream>
 
-uint32_t __stdcall init(void* args)
-{
-
+void skip() {
 	// WILL NOT WORK without this line
-	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-	
-	
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+
 	// Instructions by Dylan - https://web.archive.org/web/20190911112730/http://www.audio-surf.com/forum/index.php/topic,2885.0.html
 	HWND hwndTargetWin = FindWindowA(NULL, "Audiosurf");
-	
+
 	/*std::stringstream ss{};
 	ss << hwndTargetWin;
 	MessageBoxA(nullptr, ss.str().c_str(), "Audiosurf hWnd", MB_OK | MB_ICONERROR);*/
@@ -30,6 +28,12 @@ uint32_t __stdcall init(void* args)
 	cds.lpData = (void*)str.c_str();
 
 	SendMessage(hwndTargetWin, WM_COPYDATA, 0, (LPARAM)&cds);
+}
+
+uint32_t __stdcall init(void* args)
+{
+
+	skip();
 
 	while (true)
 		std::this_thread::sleep_for(std::chrono::milliseconds(50));
@@ -45,9 +49,10 @@ BOOL WINAPI DllMain(HMODULE handle, DWORD reason, LPVOID reserved)
 	{
 		DisableThreadLibraryCalls(handle);
 
+		
 		if (const auto thread = (HANDLE)_beginthreadex(nullptr, 0, &init, nullptr, 0, nullptr))
 		{
-			CloseHandle(thread);
+			//CloseHandle(thread);
 			return 1;
 		}
 	}
